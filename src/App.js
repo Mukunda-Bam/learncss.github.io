32899fe599f39c6913ffdc1a2f5d159c44734dbb
Mukunda-Javascript <73714951+Mukunda-python@users.noreply.github.com>
2022-04-18 17:13:05 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <main>
       <header className="App-header">
-            <h1>Learn Fundamentals of React JS</h1>
+            <h1>Learn Fundamentals of CSS</h1>
             </header>
             <nav>
             <div className='nav-bar'>
