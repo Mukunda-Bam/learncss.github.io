cb483f84cd66b7f14e261b89842bb5005d3390f4
Mukunda-Javascript <73714951+Mukunda-python@users.noreply.github.com>
2022-04-18 15:11:38 +0545

    learn css files added

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+      <header className="App-header">
+            <h1>Learn Fundamentals of React JS</h1>
+            </header>
+            <nav>
+            <div className='nav-bar'>
+               <a className='home' href='http://mukundabam.epizy.com/?i=1'>HOME</a>
+               <a className='css' href ='http:'>CSS</a>
+               
+               
+             </div>
+             </nav>
+        
+            <div>
+              <h2>  ❓ What does 'text-align: justify' do?</h2>
+              <p>In simple words, it adjusts the messy lines written in paragraph to lines having proper spacing.
+                </p>
+              
+             </div>
+             
+      </main>
+    </div>
+  );
+}
+
+export default App;
