0452563ce40f7fd39afafa2e7ca647089cb2a1ab
Mukunda Bam <73714951+Mukunda-Bam@users.noreply.github.com>
2022-05-18 17:43:29 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,9 +21,20 @@ function App() {
         
             <div>
               <h2>  ❓ What does 'text-align: justify' do?</h2>
-              <p>In simple words, it adjusts the messy lines written in paragraph to lines having proper spacing.
-                </p>
-              
+              <p>In simple words, it adjusts the messy lines written in paragraph to lines having proper spacing.</p>
+              <h2> What does ‘color’ do in CSS?</h2>
+    <p>As you are guessing, color sets the color for the elements in HTML, like the color for the text that you write.</p>
+        <h2>What is ‘background-color’ in CSS?</h2>
+    <p>Background-color sets the background color of an element like body, head, div tag, p tag, etc.</p>
+    <img src='padbodmar.png'>
+    <h2>What does ‘padding’ do in CSS?</h2>
+    <p>Padding creates space around the content of element like space around text.</p>
+    <h2>What is ‘border’ in CSS?</h2>
+        <p>Border is used for creating a border around the element. Think like a fence is created here aound that element.</p>
+        <h2>What does ‘margin’ do in CSS?</h2>
+        <p>Margin creates a space same like padding but it is around the element itself. Normally, margin is outside of borders created.</p>
+
+
              </div>
              
       </main>
